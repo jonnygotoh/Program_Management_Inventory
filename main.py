bc250dc06b9419c149a3331c037e6c0817e78d6a
jonnyanderson <jonnyanderso089@gmail.com>
2025-11-13 19:55:17 +0700

    Update

--- main.py
+++
@@ -1,8 +0,0 @@
-class data_user:
-    def __init__(self, nama, password,):
-        self.nama = nama
-        self.password = password
-
-    def info(self):
-        return f'{self.nama},{self.password}'
-    
